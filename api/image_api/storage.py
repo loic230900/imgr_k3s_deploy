4c22c4647eb9ae3ae130ec2f7b314dda01f0ac48
Waltzing <loic.waltzing@etu.unistra.fr>
2026-04-26 21:13:11 +0200

    storage: force regional S3 endpoint for presigned URLs

--- api/image_api/storage.py
+++ api/image_api/storage.py
@@ -2,7 +2,20 @@ import boto3
 import config
 from botocore.client import Config
 
-s3 = boto3.resource("s3", **config.s3, config=Config(signature_version="s3v4"))  # type: ignore
+s3_kwargs = dict(config.s3)
+
+if s3_kwargs.get("endpoint_url") is None:
+    region = s3_kwargs.get("region_name") or "us-east-1"
+    s3_kwargs["endpoint_url"] = f"https://s3.{region}.amazonaws.com"
+
+s3 = boto3.resource(
+    "s3",
+    **s3_kwargs,
+    config=Config(
+        signature_version="s3v4",
+        s3={"addressing_style": "virtual"},
+    ),
+)
 
 bucket = s3.Bucket(config.bucket_name)
 
